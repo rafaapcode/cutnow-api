d4ec9838357f4574655cc2874781c329b465d4eb
rafaapcode <rafaap2003@gmail.com>
2024-08-15 20:11:17 -0300

    feat: default configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { DatabaseModule } from './database/database.module';
+import { DatabaseService } from './database/database.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.getOrThrow('JWT_SECRET'),
+      }),
+      global: true,
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [DatabaseService, PrismaService],
 })
 export class AppModule {}
