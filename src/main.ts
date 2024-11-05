b59c83722322790ea6cddace718c68d71fa2ab68
rafaapcode <rafaap2003@gmail.com>
2024-11-04 21:56:36 -0300

    feat: adding the CORS origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://cutnow-frontend.vercel.app'],
+    origin: '*',
   });
   await app.listen(process.env.PORT, () => console.log('API RODANDO ...'));
 }
